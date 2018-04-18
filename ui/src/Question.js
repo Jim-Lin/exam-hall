f555e6b3555c0d9758d505c11cc9b2e78827fb48
Jim-Lin <acgsong.tw@yahoo.com.tw>
2018-04-18 23:52:19 +0800

    Add Single question type

--- ui/src/Question.js
+++ ui/src/Question.js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+function Single(props) {
+  const question = props.question;
+  return (
+    question.items.map((item, index) =>
+      <div key={index}>
+        <label>
+          <input type='radio' name={`radio${question.id}`} value={index} />
+          {'  '}{item.name}
+        </label>
+      </div>
+    )
+  );
+}
+
 export default class Question extends React.Component {
   constructor(props) {
     super(props);
@@ -7,10 +21,17 @@ export default class Question extends React.Component {
 
   render() {
     const question = this.props.value;
+    let questionType;
+    if (question.type == 0) {
+      questionType = ( <Single question={question} /> );
+    }
 
     return (
       <tr>
-        <td>{question.name}</td>
+        <td>
+          {question.id + 1}.<br />{question.name}<br /><br />
+          {questionType}
+        </td>
       </tr>
     );
   }
