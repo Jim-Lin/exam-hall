eb544445a3dc3508900a259319b0ab3ea5f4fa2d
Jim-Lin <acgsong.tw@yahoo.com.tw>
2018-05-01 15:12:22 +0800

    Add actions

---
+++ ui/src/actions/index.js
@@ -0,0 +1,14 @@
+export const setCountDown = status => ({
+  type: 'SET_COUNTDOWN',
+  status
+})
+
+// export const CountDownStatus = {
+//   START: true,
+//   STOP: false
+// }
+
+export const setAnswer = answer => ({
+  type: 'SET_ANSWER',
+  answer
+})
